4fc5f0dde7042a6fca68a898c8d2f6488ea4184c
morralex <morr.alex@outlook.com>
2019-10-24 21:14:17 -0700

    cutting the dice component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import Tile from "../Tile/Tile";
-// import Dice from "../Dice/Dice"
-
+import faceOne from "../../images/dice/faceOne.png";
+import faceTwo from "../../images/dice/faceTwo.png";
+import faceThree from "../../images/dice/faceThree.png";
+import faceFour from "../../images/dice/faceFour.png";
+import faceFive from "../../images/dice/faceFive.png";
+import faceSix from "../../images/dice/faceSix.png";
+import "./Board.css"
 
 class Board extends Component {
     state = {
-        tiles: Array(8).fill(null)
-
+        tiles: Array(8).fill(null),
+        face: ""
     }
     renderTile(i) {
         return (
@@ -15,6 +20,27 @@ class Board extends Component {
             />
         );
     }
+
+    setFace = () => {
+
+        let val = Math.floor(Math.random() * 6) + 1;
+
+        if (val === 1) {
+            this.setState({ current: val, face: faceOne })
+        } else if (val === 2) {
+            this.setState({ current: val, face: faceTwo })
+        } else if (val === 3) {
+            this.setState({ current: val, face: faceThree })
+        } else if (val === 4) {
+            this.setState({ current: val, face: faceFour })
+        } else if (val === 5) {
+            this.setState({ current: val, face: faceFive })
+        } else if (val === 6) {
+            this.setState({ current: val, face: faceSix })
+        }
+        console.log(val)
+    }
+
     render() {
         return (
 
@@ -35,9 +61,18 @@ class Board extends Component {
                     <div className="col s4 left">
                         {this.renderTile(3)}
                     </div>
-                    {/* <div className="col s4">
-                        {this.renderTile(1)}
-                    </div> */}
+                    <div className="col s4 center-align">
+                        <div class="card blue-grey darken-1">
+                            <div class="row card-content white-text">
+                                <div id="die-pic">
+                                    <img id="diceImg" src={this.state.face} alt={this.state.current}></img>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <button class="btn-large orange" onClick={this.setFace}>roll</button>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col s4 right">
                         {this.renderTile(7)}
                     </div>
